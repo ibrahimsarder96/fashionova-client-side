54e07b2a6596fc4d9fee9a77d8222fd32a58eac0
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-11-20 01:16:10 +0600

    add product card

--- src/Pages/Home/Products.js
+++ src/Pages/Home/Products.js
@@ -10,8 +10,8 @@ const Products = () => {
   },[])
   return (
     <div>
-      <h1 className='my-11 font-bold text-2xl ml-52'>Categories</h1>
-      <div className='mx-52 grid grid-cols-3'>
+      <h1 className='my-11 font-bold text-2xl ml-52'>LifeStyle</h1>
+      <div className='mt-11 lg:mx-52 sm:m-0 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3'>
       {
         products.map(product => <Product
         key={product.id}
