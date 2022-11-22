67f69c7f4da238bab4ebc61a3d04c38010926047
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-11-22 19:42:35 +0600

    add env.local

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import ProductDetail from "./Pages/Home/ProductDetail";
 import Navbar from "./Pages/Shared/Navbar";
+import NotFound from "./Pages/Shared/NotFound/NotFound";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path="/product/:productId" element={<ProductDetail></ProductDetail>}></Route>
-        <Route path="*"></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
