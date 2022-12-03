05da9f58d08979fca74147782f8160aa741e1b73
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-12-04 00:36:31 +0600

    giftcard style

--- src/Pages/Home/GiftCard.js
+++ src/Pages/Home/GiftCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import img1 from '../../assest/giftCard/img1.png';
+import './GiftCard.css'
+import img1 from '../../assest/giftCard/img1.jpg';
 import img2 from '../../assest/giftCard/img2.jpg';
 import img3 from '../../assest/giftCard/img3.jpg';
 import img4 from '../../assest/giftCard/img4.jpg';
@@ -11,57 +12,50 @@ const GiftCard = () => {
   return (
     <div>
      <h1 className='my-11 font-bold text-2xl ml-52'>GiftCard</h1>
-      <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 mx-52'>
+      <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6 mx-52'>
      <div>
-      <div className="hero h-42" >
-        <img className='w-56 h-56'  src={img1} alt="" />
-    <div className="hero-overlay bg-opacity-60"></div>
-    <div className="hero-content text-center text-neutral-content">
-      <div className="max-w-md">
-        <h1 className="mb-5 text-xl font-bold">Hello there</h1>
-        <p className="mb-5"></p>
-      </div>
-    </div>
+     <div className='bg-slate-200 pb-6'>
+    <div className="hero h-42">
+        <img className='w-96 h-56' src={img1} alt="" />
   </div>
-      <div className="hero h-42 my-2">
-        <img className='w-56 h-56' src={img2} alt="" />
-    <div className="hero-overlay bg-opacity-60"></div>
-    <div className="hero-content text-center text-neutral-content">
-      <div className="max-w-md">
-        <h1 className="mb-5 text-xl font-bold">Hello there</h1>
-      </div>
-    </div>
+  <div className="text-center text-xl font-bold">
+    <a href="https://www.amazon.com/">amgon</a>
   </div>
+    </div>
+    <div className='bg-slate-200 pb-6 mt-6'>
+    <div className="hero h-42">
+        <img className='w-96 h-56' src={img2} alt="" />
+  </div>
+  <div className="text-center text-xl font-bold">
+    <a href="https://www.amazon.com/">amgon</a>
+  </div>
+    </div>
      </div>
-     <div className="hero h-96">
-      <img className='h-96 w-96' src={img5} alt="" />
-    <div className="hero-overlay bg-opacity-60"></div>
-    <div className="hero-content text-center text-neutral-content">
-      <div className="max-w-md">
-        <h1 className="mb-5 text-xl font-bold">Hello there</h1>
+      <div className='bg-slate-200'>
+      <div className="hero h-96">
+      <img className='banner' src={img5} alt="" />
       </div>
-    </div>
+    <div className='pt-32 text-center text-xl font-bold'>
+    <a href="https://www.amazon.com/">amgon</a>
   </div>
-     <div>
-     <div className="hero h-42">
-      <img className='w-56 h-56' src={img3} alt="" />
-    <div className="hero-overlay bg-opacity-60"></div>
-    <div className="hero-content text-center text-neutral-content">
-      <div className="max-w-md">
-        <h1 className="mb-5 text-xl font-bold">Hello there</h1>
-        <p className="mb-5"></p>
       </div>
-    </div>
+      <div>
+     <div className='bg-slate-200 pb-6'>
+    <div className="hero h-42">
+        <img className='w-96 h-56' src={img3} alt="" />
   </div>
-     <div className="hero h-42 my-2">
-      <img className='w-56 h-56' src={img2} alt="" />
-    <div className="hero-overlay bg-opacity-60"></div>
-    <div className="hero-content text-center text-neutral-content">
-      <div className="max-w-md">
-        <h1 className="mb-5 text-xl font-bold">Hello there</h1>
-      </div>
-    </div>
+  <div className="text-center text-xl font-bold">
+    <a href="https://www.amazon.com/">amgon</a>
   </div>
+    </div>
+    <div className='bg-slate-200 pb-6 mt-6'>
+    <div className="hero h-42">
+        <img className='w-96 h-56' src={img4} alt="" />
+  </div>
+  <div className="text-center text-xl font-bold">
+    <a href="https://www.amazon.com/">amgon</a>
+  </div>
+    </div>
      </div>
    </div>
     </div>
