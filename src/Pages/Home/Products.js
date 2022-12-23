6a7450222f95098c5f33c363213e6c9aa369572c
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-12-23 21:10:51 +0600

    firebase deploy

--- src/Pages/Home/Products.js
+++ src/Pages/Home/Products.js
@@ -6,7 +6,7 @@ const Products = () => {
  
   return (
     <div>
-      <h1 className='my-11 font-bold text-2xl ml-52'>LifeStyle</h1>
+      <h1 className='my-11 font-bold text-2xl c'>LifeStyle</h1>
       <div className='mt-11 lg:mx-52 sm:m-0 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3'>
       {
         products.map(product => <Product
