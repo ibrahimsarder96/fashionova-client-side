65174a75faeda1f01d22ad4f9b71a31536cfcc30
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-12-30 01:39:28 +0600

    product

--- src/Pages/Home/Products.js
+++ src/Pages/Home/Products.js
@@ -6,8 +6,20 @@ const Products = () => {
  
   return (
     <div>
-      <h1 className='my-11 font-bold text-2xl ml-52'>LifeStyle</h1>
-      <div className='mt-11 lg:mx-52 sm:m-0 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3'>
+      <div className='flex flex-col w-full'>
+      <h1 className='mt-12 font-bold text-2xl text-center'>Our Top Collection</h1>
+      <div className='flex items-center justify-center'>
+      <div className="divider bg-orange-400 w-72 h-1 rounded-md"></div>
+      </div>
+      <p className='text-center text-xl text-purple-400'>
+        <span className='pr-8'>Best Sellers</span>
+        <span className='pr-8'>Trending</span>
+        <span>New Arrival</span>
+        </p>
+      </div>
+      
+     
+      <div className='mt-11  grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10'>
       {
         products.map(product => <Product
         key={product._id}
